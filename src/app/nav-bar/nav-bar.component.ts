d54ddd696814e8eacdb1e806ab324ae54596d44c
LohitRamachandra <tripurana.lohit@gmail.com>
2020-07-08 20:49:44 +0200

    Initial Commit

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
-  // // TEST
+
 })
 export class NavBarComponent implements OnInit {
 
